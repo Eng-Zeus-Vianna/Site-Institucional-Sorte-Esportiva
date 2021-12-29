058316361cd43c1c25ed380f4f7eb0624ba90ce5
Zeus_DevOps <58530995+Eng-Zeus-Vianna@users.noreply.github.com>
2021-12-29 23:19:01 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head><!-- Seção de cabeçalho -->
+        <meta charset="utf-8">
+        <title>Sorte Esportiva</title>
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body><!-- Seção Principal do Site -->
+        <section id="topo">
+            <?php include "topo.php"; ?>
+        </section>
+        <section id="conteudo">
+            <?php include "conteudo.php"; ?></php>
+        </section>
+        <section id=rodape">
+            <?php include "rodape.php"; ?>
+        </section>
+    </body>
+</html>
